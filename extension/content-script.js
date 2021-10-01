e340f3d5cb2918f7883cbb455bca38051b973273
Mohamed Oun <mohamed3on@gmail.com>
2021-10-01 12:22:31 +0200

    update to handle new UI

--- extension/content-script.js
+++ extension/content-script.js
@@ -7,22 +7,48 @@ const addCommas = (x) => {
   return x.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 };
 
-document.getElementById('rating_details').click();
-const fiveStarRatingsElement = document.querySelectorAll('td[width="90"]')[0].textContent;
-const oneStarRatingsElement = document.querySelectorAll('td[width="90"]')[4].textContent;
+if (document.getElementById('rating_details')) {
+  document.getElementById('rating_details').click();
+  const fiveStarRatingsElement = document.querySelectorAll('td[width="90"]')[0].textContent;
+  const oneStarRatingsElement = document.querySelectorAll('td[width="90"]')[4].textContent;
 
-document.getElementsByClassName('close')[0].click();
-const fiveStarRatingsPercentage = fiveStarRatingsElement.match(/\d*(?=%)/);
-const fiveStarRatingsAbsolute = fiveStarRatingsElement.match(/(?!\()\d+(?=\))/);
+  document.getElementsByClassName('close')[0].click();
+  const fiveStarRatingsPercentage = fiveStarRatingsElement.match(/\d*(?=%)/);
+  const fiveStarRatingsAbsolute = fiveStarRatingsElement.match(/(?!\()\d+(?=\))/);
 
-const oneStarRatingsPercentage = oneStarRatingsElement.match(/\d*(?=%)/);
-const oneStarRatingsAbsolute = oneStarRatingsElement.match(/(?!\()\d+(?=\))/);
+  const oneStarRatingsPercentage = oneStarRatingsElement.match(/\d*(?=%)/);
+  const oneStarRatingsAbsolute = oneStarRatingsElement.match(/(?!\()\d+(?=\))/);
 
-const scorePercentage = fiveStarRatingsPercentage - oneStarRatingsPercentage;
-const scoreAbsolute = fiveStarRatingsAbsolute - oneStarRatingsAbsolute;
+  const scorePercentage = fiveStarRatingsPercentage - oneStarRatingsPercentage;
+  const scoreAbsolute = fiveStarRatingsAbsolute - oneStarRatingsAbsolute;
 
-const ScoreElement = document.createElement('h1');
-ScoreElement.innerHTML = ` ${scorePercentage}% (${addCommas(String(scoreAbsolute))})`;
+  const ScoreElement = document.createElement('h1');
+  ScoreElement.innerHTML = ` ${scorePercentage}% (${addCommas(String(scoreAbsolute))})`;
 
-const Headline = document.getElementById('bookTitle');
-ScoreElement.appendAfter(Headline);
+  const Headline = document.getElementById('bookTitle');
+  ScoreElement.appendAfter(Headline);
+} else {
+  const fiveStarRatings = document
+    .querySelectorAll('div.RatingsHistogram__labelTotal')[0]
+    .textContent.replace(/,/g, '');
+
+  const oneStarRatings = document
+    .querySelectorAll('div.RatingsHistogram__labelTotal')[4]
+    .textContent.replace(/,/g, '');
+
+  const ratingsElement = document.querySelector(
+    '.RatingStatistics__meta > [data-testid="ratingsCount"]'
+  ).textContent;
+
+  const ratingsCount = ratingsElement.match(/^(\d+|\d{1,3}(,\d{3})*)(\.\d+)?(?=\s+ratings)/g)?.[0];
+  const totalRatings = parseFloat(ratingsCount.replace(/,/g, ''));
+
+  const scoreAbsolute = fiveStarRatings - oneStarRatings;
+  const scorePercentage = (scoreAbsolute / totalRatings).toFixed(2) * 100;
+
+  const ScoreElement = document.createElement('h1');
+  ScoreElement.innerHTML = ` ${scorePercentage}% (${addCommas(String(scoreAbsolute))})`;
+
+  const Headline = document.getElementsByClassName('Text Text__title1')?.[0];
+  ScoreElement.appendAfter(Headline);
+}
